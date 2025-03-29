57af897b12b6c1c9e6a4eb5fa133fef48d373744
Akarui <minezicabr0@gmail.com>
2025-03-29 11:24:31 -0300

    feat(routes): add an array to the get method

--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@ const express = require("express");
 
 const app = express();
 
+const itens = ["Backpack", "Flashlight", "Ration", "Axe"];
+
 //localhost:5000 or any port that i want to use
 app.listen("5000", () => {
     console.log("Running at http://localhost:5000");
 });
 
-app.get("/", (req, res) => {
-    res.json({
-        name: "robert",
-        age: 12
-    });
+app.get("/itens", (req, res) => {
+    res.json(itens);
 });
 
 // app.post("/receba", (req, res) => {
